4f543d9a21766f59c1e3529e66b9d117cd53aaa6
Tobin801 <tand6053@740s-pl-103.local>
2015-10-07 13:31:12 -0600

    Added Viewer class and solved null error

---
+++ src/lists/view/ListViewerThing.java
@@ -0,0 +1,11 @@
+package lists.view;
+
+import javax.swing.JOptionPane;
+
+public class ListViewerThing
+{
+	public void showList(String currentWords)
+	{
+		JOptionPane.showMessageDialog(null,currentWords);
+	}
+}
